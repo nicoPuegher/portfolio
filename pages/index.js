c36ba397abb365649e19cffb80c2f5ad370f3091
Nico <nicopuegher@gmail.com>
2023-07-26 21:29:25 -0300

    add welcome container in pages/index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react';
+
 const Page = () => {
-  return <div>Hello World</div>;
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="black" p={3} mb={4} align="center">
+        Hey there, nice to meet you!
+      </Box>
+
+      <Box>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Nicolas Puegher
+          </Heading>
+          <p>Frontend Developer</p>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Page;
