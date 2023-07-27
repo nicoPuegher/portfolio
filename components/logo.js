4e9997ee5e1b94f3aa2cec19fe14b7444cac9992
Nico <nicopuegher@gmail.com>
2023-07-27 17:22:37 -0300

    create styled span for logo component

---
+++ components/logo.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import { Text, useColorModeValue } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+const LogoBox = styled.span`
+  font-size: 1.125rem;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
