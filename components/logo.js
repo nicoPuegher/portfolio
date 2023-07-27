c2d23a756f892e2e3f00bbcd20ef80a75a52ad2d
Nico <nicopuegher@gmail.com>
2023-07-27 17:36:54 -0300

    update logo component color values

--- components/logo.js
+++ components/logo.js
@@ -13,7 +13,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <LogoBox>
-        <Text color={useColorModeValue('gray.800', 'whiteAlpha.900')}>
+        <Text color={useColorModeValue('blackAlpha.800', 'whiteAlpha.900')}>
           Nicolas Puegher
         </Text>
       </LogoBox>
