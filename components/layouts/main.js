f6d53ffc7d46b13b55320374c49b53eeb1f97dbf
Nico <nicopuegher@gmail.com>
2023-07-26 21:05:52 -0300

    initial project setup

---
+++ components/layouts/main.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import { Box, Container } from '@chakra-ui/react';
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Nicolas Puegher - Portfolio</title>
+      </Head>
+
+      <Container maxW="container.md">{children}</Container>
+    </Box>
+  );
+};
+
+export default Main;
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+const options = {
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  trailingComma: 'all',
+  semi: true,
+  endOfLine: 'lf',
+};
+
+module.exports = options;
