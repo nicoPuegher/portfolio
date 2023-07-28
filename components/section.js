f4235030df6a75c0d370d021d07c27a791c4c387
Nico <nicopuegher@gmail.com>
2023-07-28 20:24:24 -0300

    create styled section component with motion

--- components/section.js
+++ components/section.js
@@ -1,8 +1,21 @@
 import { chakra, shouldForwardProp } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 
-const StyledDiv = chakra(motion.div, {
+const StyledSection = chakra(motion.section, {
   shouldForwardProp: prop => {
     return shouldForwardProp(prop) || prop === 'transition';
   },
 });
+
+const Section = ({ children, delay = 0, ...props }) => (
+  <StyledSection
+    initial={{ y: 10, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.7, delay }}
+    {...props}
+  >
+    {children}
+  </StyledSection>
+);
+
+export default Section;
