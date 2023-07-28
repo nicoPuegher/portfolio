0776d844e3ab2697de6790eecfd6a54bffcc5348
Nico <nicopuegher@gmail.com>
2023-07-28 20:31:02 -0300

    add my experience section in pages/about

--- pages/about.js
+++ pages/about.js
@@ -17,6 +17,32 @@ const About = () => {
           impact an individual&apos;s life, myself included.
         </Paragraph>
       </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          My experience
+        </Heading>
+        <Paragraph>
+          I have been learning software engineering for almost four years now,
+          and all I can say is that I love it. My experience comes from two
+          different positions where I faced many challenges, learned many
+          things, and met amazing people.
+        </Paragraph>
+        <br />
+        <Paragraph>
+          Half of that was as a full-stack developer working on-site in
+          Copenhagen, Denmark. I spent most of my time working on the
+          application&apos;s UI and the backend, primarily with the database and
+          payment processor.
+        </Paragraph>
+        <br />
+        <Paragraph>
+          The other half was as a software engineer for a US-based company,
+          remotely from Buenos Aires, Argentina. Aside from building the UI from
+          Figma files, I had to work simultaneously on the application&apos;s
+          authentication and unit tests.
+        </Paragraph>
+      </Section>
     </>
   );
 };
