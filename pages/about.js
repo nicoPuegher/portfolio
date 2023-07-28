884deede938b31ff787fce1956069c54502e5011
Nico <nicopuegher@gmail.com>
2023-07-28 20:30:26 -0300

    add about myself section in pages/about

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,24 @@
+import Section from '../components/section';
+import { Heading } from '@chakra-ui/react';
+import Paragraph from '../components/paragraph';
+
 const About = () => {
-  return <></>;
+  return (
+    <>
+      <Section delay={0.1} mb={6}>
+        <Heading as="h3" variant="section-title">
+          About myself
+        </Heading>
+        <Paragraph>
+          I am a solution-oriented frontend developer, a lifelong self-taught
+          student, and a software engineering enthusiast. I enjoy development
+          because of the satisfaction I get by overcoming challenges. I am
+          motivated by the opportunity that software provides to positively
+          impact an individual&apos;s life, myself included.
+        </Paragraph>
+      </Section>
+    </>
+  );
 };
 
 export default About;
