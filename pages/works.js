18bef1084a075f5f0290a66f50f024f8bfe05eb3
Nico <nicopuegher@gmail.com>
2023-07-29 15:03:07 -0300

    create pages/work

--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,7 @@
-import { Container, Heading } from '@chakra-ui/react';
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react';
 import Section from '../components/section';
+import GridItem from '../components/grid-item';
+import trackyThumb from '../public/images/works/tracky.png';
 
 const Works = () => {
   return (
@@ -8,6 +10,15 @@ const Works = () => {
         <Heading as="h3" variant="section-title">
           Works
         </Heading>
+
+        <SimpleGrid>
+          <GridItem
+            id="tracky"
+            title="Tracky"
+            thumbnail={trackyThumb}
+            description="Fully responsive expense tracker frontend web application"
+          />
+        </SimpleGrid>
       </Section>
     </Container>
   );
