21447624caf64a4f70b8e3d7140cbfcfa05f3e92
Nico <nicopuegher@gmail.com>
2023-07-29 20:21:47 -0300

    clean up website function in pages/_app

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,13 @@ import Fonts from '../components/fonts';
 import Layout from '../components/layouts/main';
 import theme from '../lib/theme';
 
-const Website = ({ Component, pageProps, router }) => {
-  return (
-    <ChakraProvider theme={theme}>
-      <Fonts />
-      <Layout router={router}>
-        <Component {...pageProps} key={router.route} />
-      </Layout>
-    </ChakraProvider>
-  );
-};
+const Website = ({ Component, pageProps, router }) => (
+  <ChakraProvider theme={theme}>
+    <Fonts />
+    <Layout router={router}>
+      <Component {...pageProps} key={router.route} />
+    </Layout>
+  </ChakraProvider>
+);
 
 export default Website;
