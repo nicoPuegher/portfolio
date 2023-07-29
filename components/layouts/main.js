115e8b889c67fbb9b51939efbe173f925082b8ef
Nico <nicopuegher@gmail.com>
2023-07-29 20:23:40 -0300

    clean up main function in components/layouts/main

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,22 +1,18 @@
 import Head from 'next/head';
+import { Container, Box } from '@chakra-ui/react';
 import Navbar from '../navbar';
-import { Box, Container } from '@chakra-ui/react';
 
-const Main = ({ children, router }) => {
-  return (
-    <Box as="main">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Nicolas Puegher - Portfolio</title>
-      </Head>
-
-      <Navbar path={router.asPath} />
-
-      <Container maxW="container.md" pt={24} pb="calc(96px - 56px)">
-        {children}
-      </Container>
-    </Box>
-  );
-};
+const Main = ({ children, router }) => (
+  <Box as="main">
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Nicolas Puegher - Portfolio</title>
+    </Head>
+    <Navbar path={router.asPath} />
+    <Container maxW="container.md" pt={24} pb="calc(96px - 56px)">
+      {children}
+    </Container>
+  </Box>
+);
 
 export default Main;
