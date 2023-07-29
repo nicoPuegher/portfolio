e367e34dcc80a742235e19f1e67fa12f396961c8
Nico <nicopuegher@gmail.com>
2023-07-29 19:33:11 -0300

    move my experience section to its own component in pages/about

--- pages/about.js
+++ pages/about.js
@@ -17,33 +17,36 @@ const Myself = () => (
   </Section>
 );
 
+const Experience = () => (
+  <Section delay={0.3}>
+    <Heading as="h3" variant="section-title">
+      My experience
+    </Heading>
+    <Paragraph mb="12px">
+      I have been learning software engineering for almost four years now, and
+      all I can say is that I love it. My experience comes from two different
+      positions where I faced many challenges, learned many things, and met
+      amazing people.
+    </Paragraph>
+    <Paragraph mb="12px">
+      Half of that was as a full-stack developer working on-site in Copenhagen,
+      Denmark. I spent most of my time working on the application&apos;s UI and
+      the backend, primarily with the database and payment processor.
+    </Paragraph>
+    <Paragraph>
+      The other half was as a software engineer for a US-based company, remotely
+      from Buenos Aires, Argentina. Aside from building the UI from Figma files,
+      I had to work simultaneously on the application&apos;s authentication and
+      unit tests.
+    </Paragraph>
+  </Section>
+);
+
 const About = () => {
   return (
     <Container>
       <Myself />
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          My experience
-        </Heading>
-        <Paragraph mb="12px">
-          I have been learning software engineering for almost four years now,
-          and all I can say is that I love it. My experience comes from two
-          different positions where I faced many challenges, learned many
-          things, and met amazing people.
-        </Paragraph>
-        <Paragraph mb="12px">
-          Half of that was as a full-stack developer working on-site in
-          Copenhagen, Denmark. I spent most of my time working on the
-          application&apos;s UI and the backend, primarily with the database and
-          payment processor.
-        </Paragraph>
-        <Paragraph>
-          The other half was as a software engineer for a US-based company,
-          remotely from Buenos Aires, Argentina. Aside from building the UI from
-          Figma files, I had to work simultaneously on the application&apos;s
-          authentication and unit tests.
-        </Paragraph>
-      </Section>
+      <Experience />
     </Container>
   );
 };
