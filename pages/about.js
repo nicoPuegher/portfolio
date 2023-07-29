61c52f28372641c3ccaebec7e02d6b25c1c5dcca
Nico <nicopuegher@gmail.com>
2023-07-29 19:32:08 -0300

    move about myself section to its own component in pages/about

--- pages/about.js
+++ pages/about.js
@@ -2,22 +2,25 @@ import Section from '../components/section';
 import { Container, Heading } from '@chakra-ui/react';
 import Paragraph from '../components/paragraph';
 
+const Myself = () => (
+  <Section delay={0.1} mb={6}>
+    <Heading as="h3" variant="section-title">
+      About myself
+    </Heading>
+    <Paragraph>
+      I am a solution-oriented frontend developer, a lifelong self-taught
+      student, and a software engineering enthusiast. I enjoy development
+      because of the satisfaction I get by overcoming challenges. I am motivated
+      by the opportunity that software provides to positively impact an
+      individual&apos;s life, myself included.
+    </Paragraph>
+  </Section>
+);
+
 const About = () => {
   return (
     <Container>
-      <Section delay={0.1} mb={6}>
-        <Heading as="h3" variant="section-title">
-          About myself
-        </Heading>
-        <Paragraph>
-          I am a solution-oriented frontend developer, a lifelong self-taught
-          student, and a software engineering enthusiast. I enjoy development
-          because of the satisfaction I get by overcoming challenges. I am
-          motivated by the opportunity that software provides to positively
-          impact an individual&apos;s life, myself included.
-        </Paragraph>
-      </Section>
-
+      <Myself />
       <Section delay={0.3}>
         <Heading as="h3" variant="section-title">
           My experience
