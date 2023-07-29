73cf271bbab685fb7a6b93065213c39608bb2ce0
Nico <nicopuegher@gmail.com>
2023-07-29 17:47:58 -0300

    add project list in pages/works/tracky

--- pages/works/tracky.js
+++ pages/works/tracky.js
@@ -1,7 +1,8 @@
-import { Container, Badge } from '@chakra-ui/react';
+import { Container, Badge, List, ListItem, Link, Wrap } from '@chakra-ui/react';
 import Section from '../../components/section';
-import { WorkTitle } from '../../components/work';
+import { WorkTitle, WorkBadge, StackBadge } from '../../components/work';
 import Paragraph from '../../components/paragraph';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const Tracky = () => {
   return (
@@ -26,6 +27,48 @@ const Tracky = () => {
           case, I wanted to learn Tailwind CSS, Material UI, and build my
           project with Vite.
         </Paragraph>
+
+        <List mt={3} mb={6}>
+          <ListItem mb={0.5}>
+            <WorkBadge>Demo</WorkBadge>
+            <Link
+              href="https://nicopuegher.github.io/expenses-tracker/"
+              target="_blank"
+            >
+              Live demo application
+              <ExternalLinkIcon ml={1} />
+            </Link>
+          </ListItem>
+
+          <ListItem mb={0.5}>
+            <WorkBadge>Source</WorkBadge>
+            <Link
+              href="https://github.com/nicoPuegher/expenses-tracker"
+              target="_blank"
+            >
+              GitHub repository
+              <ExternalLinkIcon ml={1} />
+            </Link>
+          </ListItem>
+
+          <ListItem display="inline-flex">
+            <Wrap spacing={1}>
+              <WorkBadge>Stack</WorkBadge>
+              <StackBadge>JavaScript</StackBadge>
+              <StackBadge>ReactJS</StackBadge>
+              <StackBadge>Tailwind CSS</StackBadge>
+              <StackBadge>Hero Icons</StackBadge>
+              <StackBadge>Headless UI</StackBadge>
+              <StackBadge>Material UI</StackBadge>
+              <StackBadge>DayJS</StackBadge>
+              <StackBadge>Lodash</StackBadge>
+              <StackBadge>Prop Types</StackBadge>
+              <StackBadge>ESLint</StackBadge>
+              <StackBadge>Prettier</StackBadge>
+              <StackBadge>Vite</StackBadge>
+            </Wrap>
+          </ListItem>
+        </List>
       </Section>
     </Container>
   );
