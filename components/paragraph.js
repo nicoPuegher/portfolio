eacc94c94279f0c10c25c7193b034bf9755b8b3a
Nico <nicopuegher@gmail.com>
2023-07-29 17:32:19 -0300

    update paragraph commponent to use props

--- components/paragraph.js
+++ components/paragraph.js
@@ -1,8 +1,12 @@
 import styled from '@emotion/styled';
 
 const Paragraph = styled.p`
+  font-weight: ${props => props.fontWeight};
+  font-style: ${props => props.fontStyle};
+  margin-bottom: ${props => props.mb};
   text-align: justify;
   hyphens: auto;
+  opacity: ${props => props.opacity};
 `;
 
 export default Paragraph;
