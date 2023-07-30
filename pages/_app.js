d221227d1388bd4f128f708523961c1b84a6803a
Nico <nicopuegher@gmail.com>
2023-07-30 01:14:05 -0300

    remove extra layout animation

--- pages/_app.js
+++ pages/_app.js
@@ -4,25 +4,11 @@ import Layout from '../layouts/main';
 import { AnimatePresence } from 'framer-motion';
 import theme from '../lib/theme';
 
-if (typeof window !== 'undefined') {
-  window.history.scrollRestoration = 'manual';
-}
-
 const Website = ({ Component, pageProps, router }) => (
   <ChakraProvider theme={theme}>
     <Fonts />
     <Layout router={router}>
-      <AnimatePresence
-        mode="wait"
-        initial={true}
-        onExitComplete={() => {
-          if (typeof window !== 'undefined') {
-            window.scrollTo({ top: 0 });
-          }
-        }}
-      >
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
+      <Component {...pageProps} key={router.route} />
     </Layout>
   </ChakraProvider>
 );
