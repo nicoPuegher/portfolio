10a8fc8896019c594da8ca98eeccbb8dd17f0b8d
Nico <nicopuegher@gmail.com>
2023-07-30 00:59:30 -0300

    update website function to use framer motion in pages/_app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Fonts from '../components/fonts';
-import Layout from '../components/layouts/main';
+import Layout from '../layouts/main';
+import { AnimatePresence } from 'framer-motion';
 import theme from '../lib/theme';
+
+if (typeof window !== 'undefined') {
+  window.history.scrollRestoration = 'manual';
+}
 
 const Website = ({ Component, pageProps, router }) => (
   <ChakraProvider theme={theme}>
     <Fonts />
     <Layout router={router}>
-      <Component {...pageProps} key={router.route} />
+      <AnimatePresence
+        mode="wait"
+        initial={true}
+        onExitComplete={() => {
+          if (typeof window !== 'undefined') {
+            window.scrollTo({ top: 0 });
+          }
+        }}
+      >
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
     </Layout>
   </ChakraProvider>
 );
