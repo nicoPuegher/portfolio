877bb6b6faa987328daf69a59bea6771e235989f
Nicolas <nicopuegher@gmail.com>
2024-06-26 12:21:21 -0300

    Add an alias for different folders in vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@components': '/src/components',
+			'@layouts': '/src/layouts',
+			'@pages': '/src/pages',
+		},
+	},
 });
