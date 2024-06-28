36171a15bba50d76d08f8f890d53ce3658810e53
Nicolas <nicopuegher@gmail.com>
2024-06-28 12:54:01 -0300

    Import and call useDynamicMargin custom hook in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import useDynamicMargin from '@hooks/useDynamicMargin.js';
+
 import Navbar from '@layouts/Navbar.jsx';
 import Page from '@layouts/Page.jsx';
 
 export default function App() {
+	useDynamicMargin();
+
 	return (
 		<>
 			<Navbar />
