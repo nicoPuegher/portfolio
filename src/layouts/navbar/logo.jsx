09f693a3aaa5cec5d9efc21f5e3720001e17210a
Nicolas <nicopuegher@gmail.com>
2024-07-04 12:17:57 -0300

    Move logo component to its own file

---
+++ src/layouts/navbar/logo.jsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button/index.js';
+
+export default function Logo() {
+	return (
+		<Button className="pl-0 text-base font-semibold" variant="link">
+			Nicolas Puegher
+		</Button>
+	);
+}
