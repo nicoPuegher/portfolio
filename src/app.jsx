8ac6fa3243572142aa40df312a72f30a11bf0611
Nicolas <nicopuegher@gmail.com>
2024-07-05 15:42:56 -0300

    Add the context provider to the entire application

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,16 @@
 import useDynamicMargin from '@hooks/useDynamicMargin.js';
 
-import { Navbar } from './layouts/navbar/index.js';
+import ContextProvider from '@store/provider.jsx';
+import { Navbar } from '@layouts/navbar/index.js';
 import Page from '@layouts/page.jsx';
 
 export default function App() {
 	useDynamicMargin();
 
 	return (
-		<>
+		<ContextProvider>
 			<Navbar />
 			<Page />
-		</>
+		</ContextProvider>
 	);
 }
