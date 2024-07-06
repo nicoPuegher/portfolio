d682e2604381e7b6d8d326dbb49e2b07766d0c26
Nicolas <nicopuegher@gmail.com>
2024-07-06 17:23:25 -0300

    Make home button render the home page on click

--- src/layouts/navbar/logo.jsx
+++ src/layouts/navbar/logo.jsx
@@ -1,8 +1,18 @@
+import { useContext } from 'react';
+
+import { Context } from '@/store/context';
+
 import { Button } from '@/components/ui/button/index.js';
 
 export default function Logo() {
+	const context = useContext(Context);
+
 	return (
-		<Button className="pl-0 text-base font-semibold" variant="link">
+		<Button
+			className="pl-0 text-base font-semibold"
+			variant="link"
+			onClick={() => context.handleChangePage('home')}
+		>
 			Nicolas Puegher
 		</Button>
 	);
