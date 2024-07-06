99ee7679296f56e2ffdc8945827a8e3a7a959174
Nicolas <nicopuegher@gmail.com>
2024-07-06 17:26:33 -0300

    Make the about button render the about page on click

--- src/layouts/navbar/menu.jsx
+++ src/layouts/navbar/menu.jsx
@@ -1,9 +1,20 @@
+import { useContext } from 'react';
+
+import { Context } from '@/store/context.js';
+
 import { Button } from '@/components/ui/button/index.js';
 
 export default function Menu() {
+	const context = useContext(Context);
+
 	return (
 		<div className="inline-flex gap-2">
-			<Button variant="ghost">About</Button>
+			<Button
+				variant="ghost"
+				onClick={() => context.handleChangePage('about')}
+			>
+				About
+			</Button>
 			<Button variant="ghost">Works</Button>
 			<Button variant="ghost">Linkedin</Button>
 			<Button variant="ghost">Github</Button>
