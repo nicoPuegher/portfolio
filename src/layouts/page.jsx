3f2e376056102478e25bbb32515cd9792d3e3394
Nicolas <nicopuegher@gmail.com>
2024-07-07 17:30:32 -0300

    Add dummy pages

--- src/layouts/page.jsx
+++ src/layouts/page.jsx
@@ -1,3 +1,15 @@
+import { useContext } from 'react';
+
+import { Context } from '@/store/context.js';
+
 export default function Page() {
-	return <main className="mx-auto max-w-3xl flex-1 bg-red-50">Page</main>;
+	const context = useContext(Context);
+
+	return (
+		<main className="mx-auto max-w-3xl flex-1 bg-red-50">
+			{context.page === 'home' && <span>Home</span>}
+			{context.page === 'about' && <span>About</span>}
+			{context.page === 'works' && <span>Works</span>}
+		</main>
+	);
 }
