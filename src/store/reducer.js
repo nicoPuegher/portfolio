7b39ce199507ebe0e49f7f4a5f25dc9aa34ea2ad
Nicolas <nicopuegher@gmail.com>
2024-07-07 17:42:18 -0300

    Update action type change_theme reducer logic in the context

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,11 @@ export default function reducer(state, action) {
 	const newState = { ...state };
 
 	if (action.type === 'theme_change') {
-		newState.theme = action.payload;
+		if (action.payload === 'light') {
+			newState.theme = 'dark';
+		} else {
+			newState.theme = 'light';
+		}
 	}
 
 	if (action.type === 'page_change') {
