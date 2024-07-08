f01c113510d7a28dfe2cbde8d23afd78f5a71159
Nicolas <nicopuegher@gmail.com>
2024-07-08 14:55:18 -0300

    Add a div container for the theme button and dropdown menu

--- src/layouts/navbar/navbar.jsx
+++ src/layouts/navbar/navbar.jsx
@@ -1,14 +1,18 @@
 import Logo from './logo.jsx';
-import Menu from './menu.jsx';
-import ThemeButton from './themebutton.jsx';
+import MenuButtons from './menuButtons.jsx';
+import ThemeButton from './themeButton.jsx';
+import MobileMenuButtons from './mobileMenuButtons.jsx';
 
 export default function Navbar() {
 	return (
 		<header className="fixed top-0 w-full">
 			<nav className="mx-auto flex max-w-3xl justify-between py-2">
 				<Logo />
-				<Menu />
-				<ThemeButton />
+				<MenuButtons />
+				<div>
+					<ThemeButton />
+					<MobileMenuButtons />
+				</div>
 			</nav>
 		</header>
 	);
