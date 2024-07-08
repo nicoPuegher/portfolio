d062bf00aa9d0e3365ae72ff6d42b17aa38c45c2
Nicolas <nicopuegher@gmail.com>
2024-07-08 14:54:14 -0300

    Hide mobile menu buttons outside of mobile view

--- src/layouts/navbar/mobileMenuButtons.jsx
+++ src/layouts/navbar/mobileMenuButtons.jsx
@@ -4,7 +4,7 @@ import {
 	DropdownMenuContent,
 	DropdownMenuGroup,
 	DropdownMenuItem,
-} from '@/components/ui/dropdown-menu.jsx';
+} from '@/components/ui/dropdownMenu.jsx';
 
 import Button from '@/components/ui/button/button.jsx';
 
@@ -14,7 +14,11 @@ export default function mobileMenuButtons() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
+				<Button
+					className="ml-2 md:hidden"
+					variant="outline"
+					size="icon"
+				>
 					<Menu className="h-4 w-4" />
 				</Button>
 			</DropdownMenuTrigger>
