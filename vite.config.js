6f7d095eaf7a857cbe923b49c988682788a667a0
Nicolas <nicopuegher@gmail.com>
2024-07-10 12:06:54 -0300

    Add lib directory to the absolute path

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
 			'@components': path.resolve(__dirname, './src/components'),
 			'@hooks': path.resolve(__dirname, './src/hooks'),
 			'@layouts': path.resolve(__dirname, './src/layouts'),
+			'@lib': path.resolve(__dirname, './src/lib'),
 			'@pages': path.resolve(__dirname, './src/pages'),
 			'@store': path.resolve(__dirname, './src/store'),
 		},
