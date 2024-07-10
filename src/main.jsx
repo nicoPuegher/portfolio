5e20b5e1b020f52ff8ff4b772db10aa48207f910
Nicolas <nicopuegher@gmail.com>
2024-07-10 12:08:10 -0300

    Add ChakraProvider to the entire app and pass it the custom theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './app.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@lib/theme';
 
-import './assets/index.css';
+import App from './app';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<ChakraProvider theme={theme}>
+			<App />
+		</ChakraProvider>
 	</StrictMode>,
 );
