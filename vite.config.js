ff57f33d7e6b5d545771e42c0d6000ca28498377
Nicolas <nicopuegher@gmail.com>
2024-07-12 14:12:32 -0300

    Rename variables in vite.config to compile with eslint rules

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,20 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@components': path.resolve(__dirname, './src/components'),
-			'@hooks': path.resolve(__dirname, './src/hooks'),
-			'@layouts': path.resolve(__dirname, './src/layouts'),
-			'@lib': path.resolve(__dirname, './src/lib'),
-			'@pages': path.resolve(__dirname, './src/pages'),
-			'@store': path.resolve(__dirname, './src/store'),
+			'@': path.resolve(dirname, './src'),
+			'@components': path.resolve(dirname, './src/components'),
+			'@hooks': path.resolve(dirname, './src/hooks'),
+			'@layouts': path.resolve(dirname, './src/layouts'),
+			'@lib': path.resolve(dirname, './src/lib'),
+			'@pages': path.resolve(dirname, './src/pages'),
+			'@store': path.resolve(dirname, './src/store'),
 		},
 	},
 });
