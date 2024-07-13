c12c601214b60aa8807b7aae2d94309123df82de
Nicolas <nicopuegher@gmail.com>
2024-07-13 14:22:13 -0300

    Remove jsx-no-target-blank eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,6 @@ module.exports = {
 	},
 	plugins: ['react-refresh'],
 	rules: {
-		'react/jsx-no-target-blank': 'off',
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true },
