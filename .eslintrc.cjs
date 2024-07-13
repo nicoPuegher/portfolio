d4ee73d36b00e108592528998adb759a86ef39d9
Nicolas <nicopuegher@gmail.com>
2024-07-13 14:52:15 -0300

    Add recommended settings for react/jsx-a11y plugin in .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
 		'plugin:react/jsx-runtime',
 		'plugin:react-hooks/recommended',
 		'plugin:import/recommended',
+		'plugin:jsx-a11y/recommended',
 		'airbnb',
 		'airbnb/hooks',
 	],
