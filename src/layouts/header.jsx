67541fc0fcd3b3e9bf883f92845b3d356f9b48e6
Nicolas <nicopuegher@gmail.com>
2024-07-14 16:00:02 -0300

    Add proptypes to the header component

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import { Box } from '@chakra-ui/react';
 
+import PropTypes from 'prop-types';
+
 export default function Header({ children }) {
 	return <Box as="header">{children}</Box>;
 }
+
+Header.propTypes = {
+	children: PropTypes.node.isRequired,
+};
