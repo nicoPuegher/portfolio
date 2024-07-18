2f5076de366dda37763752b43f6a8222a4dd0e8c
Nicolas <nicopuegher@gmail.com>
2024-07-17 21:27:23 -0300

    Remove unnecessary quotes in theme

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -4,8 +4,8 @@ import '@fontsource-variable/inter';
 
 const theme = extendTheme({
 	fonts: {
-		heading: `'Inter variable', sans-serif`,
-		body: `'Inter variable', sans-serif`,
+		heading: `Inter variable, sans-serif`,
+		body: `Inter variable, sans-serif`,
 	},
 });
 
