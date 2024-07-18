62230beac0cf57de23b36dbd4dcc02c66471026a
Nicolas <nicopuegher@gmail.com>
2024-07-17 21:45:12 -0300

    Import and render navbar components in header component

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import { Box } from '@chakra-ui/react';
 
-import PropTypes from 'prop-types';
+import Nav from '@layouts/navbar/nav';
+import Logo from '@layouts/navbar/logo';
+import Menu from '@layouts/navbar/menu';
+import Theme from '@layouts/navbar/theme-toggle';
 
-export default function Header({ children }) {
-	return <Box as="header">{children}</Box>;
+export default function Header() {
+	return (
+		<Box as="header" pos="fixed" w="100%" backdropFilter="blur(10px)">
+			<Nav>
+				<Logo />
+				<Menu />
+				<Theme />
+			</Nav>
+		</Box>
+	);
 }
-
-Header.propTypes = {
-	children: PropTypes.node.isRequired,
-};
