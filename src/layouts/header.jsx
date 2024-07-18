9518ee813f848144cca6631a77038379de21190b
Nicolas <nicopuegher@gmail.com>
2024-07-18 15:48:36 -0300

    Import navbar components using the new index file

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 
 import { Box } from '@chakra-ui/react';
 
-import Nav from '@layouts/navbar/nav';
-import Logo from '@layouts/navbar/logo';
-import Menu from '@layouts/navbar/menu';
-import Theme from '@layouts/navbar/theme-toggle';
+import * as Navbar from '@layouts/navbar';
 
 export default function Header() {
 	return (
 		<Box as="header" pos="fixed" w="100%" backdropFilter="blur(10px)">
-			<Nav>
-				<Logo />
-				<Menu />
-				<Theme />
-			</Nav>
+			<Navbar.Nav>
+				<Navbar.Logo />
+				<Navbar.Menu />
+				<Navbar.ThemeToggle />
+			</Navbar.Nav>
 		</Box>
 	);
 }
