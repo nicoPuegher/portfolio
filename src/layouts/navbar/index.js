07bcc9827e346dba221c9d35c2cede80c2ed3f4d
Nicolas <nicopuegher@gmail.com>
2024-07-18 15:43:49 -0300

    Create an index file for all the navbar components and export them

---
+++ src/layouts/navbar/index.js
@@ -0,0 +1,5 @@
+export { default as Nav } from '@layouts/navbar/nav';
+export { default as Logo } from '@layouts/navbar/logo';
+export { default as Menu } from '@layouts/navbar/menu';
+export { default as ThemeToggle } from '@layouts/navbar/theme-toggle';
+export { default as MenuMobile } from '@layouts/navbar/menu-mobile';
