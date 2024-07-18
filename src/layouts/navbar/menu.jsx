895d57548f945ae73ba1302a489d3c9bd791bfc7
Nicolas <nicopuegher@gmail.com>
2024-07-18 15:45:14 -0300

    Hide navbar menu component on mobile and show it on tablet+

--- src/layouts/navbar/menu.jsx
+++ src/layouts/navbar/menu.jsx
@@ -4,7 +4,11 @@ import { ButtonGroup, Button } from '@chakra-ui/react';
 
 export default function Menu() {
 	return (
-		<ButtonGroup variant="ghost" spacing="3">
+		<ButtonGroup
+			display={{ base: 'none', md: 'flex' }}
+			variant="ghost"
+			spacing="3"
+		>
 			<Button>About</Button>
 			<Button>Works</Button>
 			<Button>Linkedin</Button>
