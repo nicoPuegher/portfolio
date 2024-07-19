37a992478095daca8992189f230ce6ceef79c8e9
Nicolas <nicopuegher@gmail.com>
2024-07-19 17:23:15 -0300

    Render main component in app

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '@layouts/header';
+import Main from '@layouts/main';
 
 export default function App() {
 	return (
 		<>
-			<Header>Navbar</Header>
+			<Header />
+			<Main />
 		</>
 	);
 }
