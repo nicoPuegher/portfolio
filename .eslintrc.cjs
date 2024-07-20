c87ac96e65b5ea7eb60dee4f5c78156c9c8eee9e
Nicolas <nicopuegher@gmail.com>
2024-07-20 16:34:04 -0300

    Add @pages path to .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
 					['@constants', './src/constants'],
 					['@layouts', './src/layouts'],
 					['@lib', './src/lib'],
+					['@pages', './src/pages'],
 					['@store', './src/store'],
 				],
 				extensions: ['.js', '.jsx'],
