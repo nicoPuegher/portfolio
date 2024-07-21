2acc59953e2cc638a4032591b95fa723ac1ab781
Nicolas <nicopuegher@gmail.com>
2024-07-21 17:40:09 -0300

    Style profile component

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,3 +1,18 @@
 import React from 'react';
 
-export default function Profile() {}
+import { Avatar } from '@chakra-ui/react';
+
+import profilePicture from '@assets/profile.png';
+
+export default function Profile() {
+	return (
+		<Avatar
+			name="Nicolas Puegher"
+			size="xl"
+			borderWidth={4}
+			borderStyle="solid"
+			borderColor="black"
+			src={profilePicture}
+		/>
+	);
+}
