5f8dbbe1c28b4f904a344219cddaaa7fe23d9de6
Nicolas <nicopuegher@gmail.com>
2024-07-21 20:07:32 -0300

    Create a button call component

---
+++ src/components/ui/button-call.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Button } from '@chakra-ui/react';
+
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+
+export default function ButtonCall() {
+	return <Button rightIcon={<ArrowForwardIcon />}>Check my work</Button>;
+}
