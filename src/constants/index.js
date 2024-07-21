cdc791f52975fa916d0be970166e1e86d0b42042
Nicolas <nicopuegher@gmail.com>
2024-07-21 20:03:03 -0300

    Add information about myself in the constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,9 @@
-export const developer = 'Nicolas Puegher';
+const developer = {
+	name: 'Nicolas Puegher',
+	role: 'Frontend developer',
+	greeting: 'Hey there 👋🏻, nice to meet you!',
+	quickSummary:
+		'I am a passionate developer dedicated to creating beautiful and functional web experiences. Explore my work and feel free to reach out if you have any questions or opportunities.',
+};
+
+export default developer;
