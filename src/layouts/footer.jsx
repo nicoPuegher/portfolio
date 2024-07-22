6fd55f5719e618e55c5fec0f055d5064a41827b3
Nicolas <nicopuegher@gmail.com>
2024-07-22 20:17:31 -0300

    Create a footer layout

---
+++ src/layouts/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Box, Text } from '@chakra-ui/react';
+
+export default function Footer() {
+	const date = new Date();
+	const currentYear = date.getFullYear();
+	const copyright = `© ${currentYear} Nicolas Puegher - All rights reserved.`;
+
+	return (
+		<Box as="footer" py={2} color="white" backgroundColor="black">
+			<Text textAlign="center">{copyright}</Text>
+		</Box>
+	);
+}
