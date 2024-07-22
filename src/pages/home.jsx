c5dee6d3aadee22ffe918fe5020d8fab47c4211e
Nicolas <nicopuegher@gmail.com>
2024-07-22 20:12:28 -0300

    Render components related to home page in it

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
+import { Divider, Text } from '@chakra-ui/react';
+
+import Section from '@/layouts/section';
+import Message from '@/components/message';
+import Greetings from '@/components/greetings';
+
+import developer from '@/constants';
+
 export default function Home() {
-	return <p>Home</p>;
+	return (
+		<>
+			<Message>{developer.greeting}</Message>
+			<Greetings />
+			<Divider width="50%" mx="auto" my={10} />
+			<Section title="Quick summary ">
+				<Text>{developer.quickSummary}</Text>
+			</Section>
+		</>
+	);
 }
