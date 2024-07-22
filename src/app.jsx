9d34fc708d22f3036527b8081765c1a4817749df
Nicolas <nicopuegher@gmail.com>
2024-07-22 20:18:18 -0300

    Remove context logic from app and only render semantic elements in it

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,15 @@
-import React, { useContext } from 'react';
-
-import Context from '@store/context';
+import React from 'react';
 
 import Header from '@layouts/header';
 import Main from '@layouts/main';
-
-import Home from '@pages/home';
-import About from '@pages/about';
-import Works from '@pages/works';
+import Footer from '@layouts/footer';
 
 export default function App() {
-	const context = useContext(Context);
-
 	return (
 		<>
 			<Header />
-			<Main>
-				{context.page === 'home' && <Home />}
-				{context.page === 'about' && <About />}
-				{context.page === 'works' && <Works />}
-			</Main>
+			<Main />
+			<Footer />
 		</>
 	);
 }
