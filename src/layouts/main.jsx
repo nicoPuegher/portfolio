ae23b88bd090e3beb106eaec2d67cb306e034f2b
Nicolas <nicopuegher@gmail.com>
2024-07-22 20:22:58 -0300

    Add the context logic from app in main component and create a custom box for cleaner jsx

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import Context from '@store/context';
 
 import { Box } from '@chakra-ui/react';
 
+import Home from '@pages/home';
+import About from '@pages/about';
+import Works from '@pages/works';
+
 import PropTypes from 'prop-types';
 
-export default function Main({ children }) {
-	return <Box as="main">{children}</Box>;
+export default function Main() {
+	const context = useContext(Context);
+
+	return (
+		<CustomBox>
+			{context.page === 'home' && <Home />}
+			{context.page === 'about' && <About />}
+			{context.page === 'works' && <Works />}
+		</CustomBox>
+	);
 }
 
-Main.propTypes = {
+function CustomBox({ children }) {
+	return (
+		<Box
+			as="main"
+			flexGrow="1"
+			maxW="container.md"
+			px={2}
+			py={10}
+			mx="auto"
+			display="flex"
+			flexDirection="column"
+		>
+			{children}
+		</Box>
+	);
+}
+CustomBox.propTypes = {
 	children: PropTypes.node.isRequired,
 };
