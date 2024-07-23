6886228652e65ce73304c2f223836cde194e5008
Nicolas <nicopuegher@gmail.com>
2024-07-23 20:46:39 -0300

    Reorder imports from navbar logo to compile with eslint

--- src/layouts/navbar/logo.jsx
+++ src/layouts/navbar/logo.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 
-import Context from '@/store/context';
-
 import { Heading, Link } from '@chakra-ui/react';
 
 import developer from '@constants';
+
+import Context from '@/store/context';
 
 export default function Logo() {
 	const context = useContext(Context);
