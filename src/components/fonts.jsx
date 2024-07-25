b644778f739e25a0be3e7f8f13afec950cbc0d27
Nicolas <nicopuegher@gmail.com>
2024-07-25 15:58:31 -0300

    Create a fonts component with emotion

---
+++ src/components/fonts.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Global } from '@emotion/react';
+
+export default function Fonts() {
+	return (
+		<Global
+			styles={`
+				@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
+      			body {
+        			font-family: 'Inter', sans-serif;
+      			}
+    		`}
+		/>
+	);
+}
