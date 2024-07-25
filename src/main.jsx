90ae42603c99b9f1df9db025970cd02e7c163d1d
Nicolas <nicopuegher@gmail.com>
2024-07-25 15:31:01 -0300

    Update imports order in init main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import ContextProvider from '@store/provider';
-
-import { ChakraProvider } from '@chakra-ui/react';
 import theme from '@lib/theme';
-
-import App from './app';
+import App from '@/app';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
