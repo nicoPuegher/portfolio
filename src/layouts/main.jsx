68c951125ba2c61edbbd8478b90abd4de5bf9979
Nicolas <nicopuegher@gmail.com>
2024-07-25 15:37:52 -0300

    Update imports order in main layout

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,11 +1,8 @@
 import React, { useContext } from 'react';
-
 import { Box } from '@chakra-ui/react';
-
 import PropTypes from 'prop-types';
 
 import Context from '@store/context';
-
 import Home from '@pages/home';
 import About from '@pages/about';
 import Works from '@pages/works';
