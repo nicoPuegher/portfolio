f75a8c7af85ef951c59729e8f70ea86990afe04e
Nicolas <nicopuegher@gmail.com>
2024-07-27 14:00:51 -0300

    Add context to greetings component

--- src/components/greetings.jsx
+++ src/components/greetings.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
+import Context from '@/store/context';
 import Profile from '@/components/profile';
 import Myself from '@/components/myself';
 import ButtonCall from '@/components/ui/button-call';
 
 export default function Greetings() {
+	const context = useContext(Context);
+
 	return (
 		<CustomBox>
 			<Profile />
