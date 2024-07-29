2a5d3fed66c21880e2ca055359dac9e4d294db86
Nicolas <nicopuegher@gmail.com>
2024-07-29 15:07:54 -0300

    Render about me component inside a section in about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import Section from '@/layouts/section';
+import AboutMe from '@/components/about-me';
+
 export default function About() {
-	return <p>About</p>;
+	return (
+		<>
+			<Section title="About me">
+				<AboutMe />
+			</Section>
+		</>
+	);
 }
