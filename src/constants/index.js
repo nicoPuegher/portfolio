0aaf40c5121fffde6e538459889d8cde60b22553
Nicolas <nicopuegher@gmail.com>
2024-07-29 15:05:11 -0300

    Add summary to the constants index

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,11 @@ const developer = {
 	greeting: 'Hey there 👋🏻, nice to meet you!',
 	quickSummary:
 		'I am a passionate developer dedicated to creating beautiful and functional web experiences. Explore my work and feel free to reach out if you have any questions or opportunities.',
+	summary: {
+		1: 'Dedicated frontend developer with over a year of experience in crafting high-quality web applications, solving complex problems, and delivering efficient production code.',
+		2: 'Passionate about software engineering and committed to continuous learning and the adoption of emerging technologies.',
+		3: "I enjoy development because of the satisfaction I get from overcoming challenges. I am motivated by the opportunity to positively impact individuals' lives through software.",
+	},
 };
 
 export default developer;
