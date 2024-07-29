4ac2c7d75da3f17a42bbb915dbaa18712163a443
Nicolas <nicopuegher@gmail.com>
2024-07-29 15:05:46 -0300

    Display summary in different paragraphs inside a grid in the about me component

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
+import { Grid, Text } from '@chakra-ui/react';
 
-export default function AboutMe() {}
+import developer from '@/constants';
+
+export default function AboutMe() {
+	return (
+		<Grid gap={5}>
+			<Text>{developer.summary['1']}</Text>
+			<Text>{developer.summary['2']}</Text>
+			<Text>{developer.summary['3']}</Text>
+		</Grid>
+	);
+}
