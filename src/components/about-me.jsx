48667f3bdd8b976ed3785cc3b2ca6857460f7fb6
Nicolas <nicopuegher@gmail.com>
2024-07-30 17:31:22 -0300

    Add margin bottom to the paragraphs in about me component

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Grid, Text } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
 import developer from '@/constants';
 
 export default function AboutMe() {
 	return (
-		<Grid gap={5}>
-			<Text>{developer.summary['1']}</Text>
-			<Text>{developer.summary['2']}</Text>
+		<>
+			<Text mb={5}>{developer.summary['1']}</Text>
+			<Text mb={5}>{developer.summary['2']}</Text>
 			<Text>{developer.summary['3']}</Text>
-		</Grid>
+		</>
 	);
 }
