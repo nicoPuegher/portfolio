99569d80414f936d98b6a08f2b219b9e14d37bc4
Nicolas <nicopuegher@gmail.com>
2024-07-31 17:43:11 -0300

    Render my experience component in about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Section from '@/layouts/section';
 import AboutMe from '@/components/about-me';
+import MyExperience from '@/components/my-experience';
 
 export default function About() {
 	return (
 		<>
 			<Section mb={10} title="About me">
 				<AboutMe />
+			</Section>
+			<Section title="My experience">
+				<MyExperience />
 			</Section>
 		</>
 	);
