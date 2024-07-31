e41c217d6d882493ae7b7c03fedb6d6ba1e0dca2
Nicolas <nicopuegher@gmail.com>
2024-07-31 17:56:29 -0300

    Use single quotes instead of backticks in chakraui theme for the font family

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -2,8 +2,8 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
 	fonts: {
-		heading: `Inter variable, sans-serif`,
-		body: `Inter variable, sans-serif`,
+		heading: 'Inter variable, sans-serif',
+		body: 'Inter variable, sans-serif',
 	},
 	styles: {
 		global: {
