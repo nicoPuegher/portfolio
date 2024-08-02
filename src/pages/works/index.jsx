88dcde837712305b6567e0e9d44621b07caeb408
Nicolas <nicopuegher@gmail.com>
2024-08-02 13:40:04 -0300

    Add sorting project to works page and add corresponding props

--- src/pages/works/index.jsx
+++ src/pages/works/index.jsx
@@ -3,6 +3,7 @@ import { SimpleGrid } from '@chakra-ui/react';
 
 import Section from '@/layouts/section';
 import GridItem from '@/components/grid-item';
+import sortingThumbnail from '@assets/sorting/sorting.png';
 import trackyThumbnail from '@/assets/tracky/tracky.png';
 
 export default function Works() {
@@ -10,9 +11,16 @@ export default function Works() {
 		<Section title="Works">
 			<SimpleGrid>
 				<GridItem
+					title="Sorting Visualizer"
+					description="Frontend web application that animates the sorting process of various algorithms, offering an engaging way to explore and understand how these algorithms operate."
+					src={sortingThumbnail}
+					fallback="Loading image ..."
+				/>
+				<GridItem
 					title="Tracky"
 					description="Frontend web application designed to efficiently track and manage your expenses."
 					src={trackyThumbnail}
+					fallback="Loading image ..."
 				/>
 			</SimpleGrid>
 		</Section>
