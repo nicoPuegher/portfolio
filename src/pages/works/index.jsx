d5f20a74bed1a38e93e21578ca201a9ff1dab4b4
Nicolas <nicopuegher@gmail.com>
2024-08-01 23:26:53 -0300

    Render tracky as a grid item in works page

--- src/pages/works/index.jsx
+++ src/pages/works/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { SimpleGrid } from '@chakra-ui/react';
 
 import Section from '@/layouts/section';
+import GridItem from '@/components/grid-item';
+import trackyThumbnail from '@/assets/tracky/tracky.png';
 
 export default function Works() {
 	return (
-		<>
-			<Section title="Works"></Section>
-		</>
+		<Section title="Works">
+			<SimpleGrid>
+				<GridItem
+					title="Tracky"
+					description="Frontend web application designed to efficiently track and manage your expenses."
+					src={trackyThumbnail}
+				/>
+			</SimpleGrid>
+		</Section>
 	);
 }
