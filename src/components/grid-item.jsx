c3ec0e48c6c24fda4c979ea60ac4985a9b53335d
Nicolas <nicopuegher@gmail.com>
2024-08-02 13:37:59 -0300

    Add a fallback for the image in grid item component to display it when it is loading

--- src/components/grid-item.jsx
+++ src/components/grid-item.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Box, Image, Center, Text } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-export default function GridItem({ title, description, src }) {
+export default function GridItem({ title, description, src, fallback }) {
 	return (
 		<Box>
-			<Image src={src} alt={title} borderRadius="md" shadow="xs" />
+			<Image
+				borderRadius="md"
+				src={src}
+				alt={title}
+				fallback={<Center>{fallback}</Center>}
+			/>
 			<Box>
 				<Text>{title}</Text>
 				<Text>{description}</Text>
@@ -17,4 +22,5 @@ GridItem.propTypes = {
 	title: PropTypes.string.isRequired,
 	src: PropTypes.string.isRequired,
 	description: PropTypes.string.isRequired,
+	fallback: PropTypes.string.isRequired,
 };
