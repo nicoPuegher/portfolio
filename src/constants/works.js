634b267624e7aa2e7510de8a608dacd98121d2e7
Nicolas <nicopuegher@gmail.com>
2024-08-04 13:02:35 -0300

    Create a constant file for works

---
+++ src/constants/works.js
@@ -0,0 +1,16 @@
+const works = {
+	sorting: {
+		title: 'Sorting Visualizer',
+		description:
+			'Frontend web application that animates the sorting process of various algorithms, offering an engaging way to explore and understand how these algorithms operate.',
+		fallback: 'Loading image ...',
+	},
+	tracky: {
+		title: 'Tracky',
+		description:
+			'Frontend web application designed to efficiently track and manage your expenses.',
+		fallback: 'Loading image ...',
+	},
+};
+
+export default works;
