4424d4483f3ad35caa50c5c005f8677f518fa5c4
Nicolas <nicopuegher@gmail.com>
2024-08-05 13:58:49 -0300

    Pass a goToPage prop with context to each grid-item component in works page

--- src/pages/works/index.jsx
+++ src/pages/works/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SimpleGrid, Divider } from '@chakra-ui/react';
 
+import Context from '@/store/context';
 import Section from '@/layouts/section';
 import GridItem from '@/components/grid-item';
 import sortingThumbnail from '@assets/sorting/sorting.png';
 import trackyThumbnail from '@/assets/tracky/tracky.png';
 
 export default function Works() {
+	const context = useContext(Context);
+
 	return (
 		<Section title="Works">
 			<SimpleGrid gap={10}>
@@ -15,6 +18,7 @@ export default function Works() {
 					description="Frontend web application that animates the sorting process of various algorithms, offering an engaging way to explore and understand how these algorithms operate."
 					src={sortingThumbnail}
 					fallback="Loading image ..."
+					goToPage={() => context.handleChangePage('sorting')}
 				/>
 				<Divider width="half" mx="auto" />
 				<GridItem
@@ -22,6 +26,7 @@ export default function Works() {
 					description="Frontend web application designed to efficiently track and manage your expenses."
 					src={trackyThumbnail}
 					fallback="Loading image ..."
+					goToPage={() => context.handleChangePage('tracky')}
 				/>
 			</SimpleGrid>
 		</Section>
