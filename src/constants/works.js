a8755fdb97809b6832d68e0b8d856adeb533d223
Nicolas <nicopuegher@gmail.com>
2024-08-06 17:16:05 -0300

    Update works contant to include an image source

--- src/constants/works.js
+++ src/constants/works.js
@@ -1,14 +1,19 @@
+import sortingThumbnail from '@/assets/sorting/sorting.png';
+import trackyThumbnail from '@/assets/tracky/tracky.png';
+
 const works = {
 	sorting: {
 		title: 'Sorting Visualizer',
 		description:
 			'Frontend web application that animates the sorting process of various algorithms, offering an engaging way to explore and understand how these algorithms operate.',
+		src: { sortingThumbnail },
 		fallback: 'Loading image ...',
 	},
 	tracky: {
 		title: 'Tracky',
 		description:
 			'Frontend web application designed to efficiently track and manage your expenses.',
+		src: { trackyThumbnail },
 		fallback: 'Loading image ...',
 	},
 };
