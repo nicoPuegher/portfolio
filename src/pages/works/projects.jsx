0378a2d14b5743ee4416926a1e8bc656d329a8e1
Nicolas <nicopuegher@gmail.com>
2024-08-06 17:15:12 -0300

    Generate a dynamic array of work components in projects component

--- src/pages/works/projects.jsx
+++ src/pages/works/projects.jsx
@@ -9,9 +9,30 @@ import works from '@/constants/works';
 export default function Projects() {
 	const context = useContext(Context);
 
+	const workEntries = Object.entries(works);
+
+	const projects = workEntries.map(
+		([project, { title, description, src, fallback }], index) => {
+			const isLastItem = workEntries.length - 1 === index;
+
+			return (
+				<React.Fragment key={project}>
+					<GridItem
+						title={title}
+						description={description}
+						src={Object.values(src)[0]}
+						fallback={fallback}
+						goToPage={() => context.handleChangePage(project)}
+					/>
+					{!isLastItem && <Divider width="half" mx="auto" />}
+				</React.Fragment>
+			);
+		},
+	);
+
 	return (
 		<Section title="Works">
-			<SimpleGrid gap={10}>Projects</SimpleGrid>
+			<SimpleGrid gap={10}>{projects}</SimpleGrid>
 		</Section>
 	);
 }
