d9fac09efe82ff2d5cb21b487c5a0c411ed4c615
Nicolas <nicopuegher@gmail.com>
2024-08-07 18:01:58 -0300

    Add a breadcrumb menu to navigate back to works in sorting component

--- src/pages/works/sorting.jsx
+++ src/pages/works/sorting.jsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
 
 import Context from '@/store/context';
 import Section from '@/layouts/section';
@@ -6,5 +8,25 @@ import Section from '@/layouts/section';
 export default function Sorting() {
 	const context = useContext(Context);
 
-	return <Section title="Sorting Visualizer">Sorting</Section>;
+	return (
+		<Section title="Sorting Visualizer">
+			<Breadcrumb
+				display="inline-block"
+				spacing="8px"
+				separator={<ChevronRightIcon color="gray.500" />}
+			>
+				<BreadcrumbItem>
+					<BreadcrumbLink
+						onClick={() => context.handleChangePage('works')}
+					>
+						Works
+					</BreadcrumbLink>
+				</BreadcrumbItem>
+
+				<BreadcrumbItem isCurrentPage>
+					<BreadcrumbLink>Sorting</BreadcrumbLink>
+				</BreadcrumbItem>
+			</Breadcrumb>
+		</Section>
+	);
 }
