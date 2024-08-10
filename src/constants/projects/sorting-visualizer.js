4f428beede91a99d675444fb471c789dc8cd9a77
Nicolas <nicopuegher@gmail.com>
2024-08-10 15:53:17 -0300

    Create a constant file for the sorting visualizer project information

---
+++ src/constants/projects/sorting-visualizer.js
@@ -0,0 +1,18 @@
+import preview from '@assets/projects/sorting-visualizer/preview.png';
+import mobilePreview1 from '@assets/projects/sorting-visualizer/mobile-preview1.png';
+import mobilePreview2 from '@assets/projects/sorting-visualizer/mobile-preview2.png';
+
+const sortingVisualizer = {
+	description:
+		'Frontend web application that animates the sorting process of various algorithms, offering an engaging way to explore and understand how these algorithms operate. This project demystifies complex computational concepts and brings them to life in an engaging format.',
+	features: [
+		'Algorithm Visualization: Experience the sorting process step-by-step with animated representations, allowing users to see how each algorithm operates.',
+		'Adjustable Parameters: Users can modify the sorting speed and chart size, tailoring the experience to their preferences and learning pace.',
+		'Educational Insight: The application provides an educational platform for those curious about sorting algorithms, making complex ideas accessible and understandable.',
+	],
+	closer: 'This project is ideal for students, educators, and anyone interested in computer science, offering a dynamic and interactive way to explore fundamental algorithms.',
+	preview,
+	mobilePreviews: [mobilePreview1, mobilePreview2],
+};
+
+export default sortingVisualizer;
