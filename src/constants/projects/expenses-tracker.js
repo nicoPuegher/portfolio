fb6f0164ec17571e361efd9fa47cb530ab018515
Nicolas <nicopuegher@gmail.com>
2024-08-10 15:55:38 -0300

    Add a constant file for the expenses tracker project information

---
+++ src/constants/projects/expenses-tracker.js
@@ -0,0 +1,19 @@
+import preview from '@assets/projects/expenses-tracker/preview.png';
+import mobilePreview1 from '@assets/projects/sorting-visualizer/mobile-preview1.png';
+import mobilePreview2 from '@assets/projects/sorting-visualizer/mobile-preview2.png';
+
+const expensesTracker = {
+	description:
+		'Frontend web application designed to help users efficiently manage and monitor their personal expenses. It features a user-friendly interface that allows users to add, edit, and delete expenses while categorizing them for better organization.',
+	features: [
+		'Dynamic Categorization: Users can easily assign expenses to a variety of predefined categories.',
+		'Interactive Visualizations: The application features dynamic charts that visually represent monthly expenditures, enabling users to quickly identify spending patterns and trends.',
+		'Flexible Filtering: Expenses can be filtered by year or month and are displayed in descending order, allowing users to effortlessly navigate their financial history.',
+		'Robust Form Validation: The app includes comprehensive form validation to ensure data accuracy, providing users with clear guidance when entering information.',
+	],
+	closer: 'This application offers a seamless experience for users looking to gain better control over their finances, providing insightful data through its intuitive design and powerful features.',
+	preview,
+	mobilePreviews: [mobilePreview1, mobilePreview2],
+};
+
+export default expensesTracker;
