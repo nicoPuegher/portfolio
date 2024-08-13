b3d979dc14e709d61d740e31791336bbf63afca4
Nicolas <nicopuegher@gmail.com>
2024-08-13 12:51:04 -0300

    Render a list of items inside bullet points component

--- src/components/bullet-points.jsx
+++ src/components/bullet-points.jsx
@@ -1,1 +1,15 @@
-export default function BulletPoints() {}
+import { List, ListItem, Text } from '@chakra-ui/react';
+
+export default function BulletPoints({ items }) {
+	const listItems = items.map((item) => (
+		<ListItem key={item}>
+			<Text as="i">{item}</Text>
+		</ListItem>
+	));
+
+	return (
+		<List fontSize="sm" my={5} spacing={3}>
+			{listItems}
+		</List>
+	);
+}
