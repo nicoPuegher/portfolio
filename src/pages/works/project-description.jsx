3f513510f7696c698d4b231a427aae46842bc794
Nicolas <nicopuegher@gmail.com>
2024-08-13 12:57:44 -0300

    Render text and bullet points in project description

--- src/pages/works/project-description.jsx
+++ src/pages/works/project-description.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
+import { Text } from '@chakra-ui/react';
 
-export default function ProjectDescription() {}
+import BulletPoints from '@/components/bullet-points';
+
+export default function ProjectDescription({ project }) {
+	return (
+		<>
+			<Text>{project.description}</Text>
+			<BulletPoints items={project.features} />
+			<Text>{project.closer}</Text>
+		</>
+	);
+}
