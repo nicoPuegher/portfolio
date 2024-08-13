70beabc2aeddb96317f78f7f05da9da5da60fcb0
Nicolas <nicopuegher@gmail.com>
2024-08-13 14:15:37 -0300

    Divide features into objects, add links and stack properties to sorting visualizer constant file

--- src/constants/projects/sorting-visualizer.js
+++ src/constants/projects/sorting-visualizer.js
@@ -8,11 +8,34 @@ const sortingVisualizer = {
 	description:
 		'Frontend web application that animates the sorting process of various algorithms, offering an engaging way to explore and understand how these algorithms operate. This project demystifies complex computational concepts and brings them to life in an engaging format.',
 	features: [
-		'Algorithm Visualization: Experience the sorting process step-by-step with animated representations, allowing users to see how each algorithm operates.',
-		'Adjustable Parameters: Users can modify the sorting speed and chart size, tailoring the experience to their preferences and learning pace.',
-		'Educational Insight: The application provides an educational platform for those curious about sorting algorithms, making complex ideas accessible and understandable.',
+		{
+			title: 'Algorithm Visualization: ',
+			content:
+				'Experience the sorting process step-by-step with animated representations, allowing users to see how each algorithm operates.',
+		},
+		{
+			title: 'Adjustable Parameters: ',
+			content:
+				'Users can modify the sorting speed and chart size, tailoring the experience to their preferences and learning pace.',
+		},
+		{
+			title: 'Educational Insight: ',
+			content:
+				'The application provides an educational platform for those curious about sorting algorithms, making complex ideas accessible and understandable.',
+		},
 	],
 	closer: 'This project is ideal for students, educators, and anyone interested in computer science, offering a dynamic and interactive way to explore fundamental algorithms.',
+	link: 'sorting-visualizer',
+	stack: [
+		'HTML5',
+		'CSS3',
+		'JavaScript',
+		'React',
+		'Proptypes',
+		'Prettier',
+		'ESLint',
+		'Vite',
+	],
 	fallback: 'Loading image...',
 	preview,
 	mobilePreviews: [mobilePreview1, mobilePreview2],
