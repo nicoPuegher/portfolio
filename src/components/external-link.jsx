5cd11143e92e76052ab4ee45dfc6e65cd5848e9c
Nicolas <nicopuegher@gmail.com>
2024-08-13 13:19:16 -0300

    Add a url and text prop to external link component

--- src/components/external-link.jsx
+++ src/components/external-link.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Link } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
-export default function ExternalLink() {}
+export default function ExternalLink({ url, text }) {
+	return (
+		<Link href={url} isExternal>
+			{text} <ExternalLinkIcon mx={2} />
+		</Link>
+	);
+}
