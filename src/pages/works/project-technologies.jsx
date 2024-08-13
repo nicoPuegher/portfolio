dee1a6c42dfef255ebe196816b39e6529487eb34
Nicolas <nicopuegher@gmail.com>
2024-08-13 14:13:54 -0300

    Render a list of tags in project technologies

--- src/pages/works/project-technologies.jsx
+++ src/pages/works/project-technologies.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
+import { Tag, ListItem, Box } from '@chakra-ui/react';
 
-export default function ProjectTechnologies() {}
+export default function ProjectTechnologies({ stack }) {
+	const technologies = stack.map((item) => <Tag key={item}>{item}</Tag>);
+
+	return (
+		<ListItem>
+			<Box display="flex" flexWrap="wrap" justifyContent="center" gap={1}>
+				{technologies}
+			</Box>
+		</ListItem>
+	);
+}
