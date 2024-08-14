2a951e81b6b897247ac071a9c6bdbe35f5194253
Nicolas <nicopuegher@gmail.com>
2024-08-14 16:21:49 -0300

    Render all project elements in the expenses tracker works page

--- src/pages/works/expenses-tracker/index.jsx
+++ src/pages/works/expenses-tracker/index.jsx
@@ -1,3 +1,31 @@
 import React from 'react';
+import { List } from '@chakra-ui/react';
 
-export default function ExpensesTracker() {}
+import Section from '@/layouts/section';
+import expensesTracker from '@/constants/projects/expenses-tracker';
+import Breadcrumb from '@/pages/works/breadcrumb';
+import ProjectDescription from '@/pages/works/project-description';
+import ProjectLinks from '@/pages/works/project-links';
+import ProjectTechnologies from '@/pages/works/project-technologies';
+import ProjectImages from '@/pages/works/project-images';
+
+export default function ExpensesTracker() {
+	return (
+		<Section title={expensesTracker.title}>
+			<Breadcrumb
+				name={expensesTracker.title}
+				year={expensesTracker.year}
+			/>
+			<ProjectDescription project={expensesTracker} />
+			<List>
+				<ProjectLinks url={expensesTracker.link} />
+				<ProjectTechnologies stack={expensesTracker.stack} />
+			</List>
+			<ProjectImages
+				preview={expensesTracker.preview}
+				mobilePreview1={expensesTracker.mobilePreviews[0]}
+				mobilePreview2={expensesTracker.mobilePreviews[1]}
+			/>
+		</Section>
+	);
+}
