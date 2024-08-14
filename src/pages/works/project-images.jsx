bb8fbe5155a3fd17cbc51422a8a6a9962372a315
Nicolas <nicopuegher@gmail.com>
2024-08-14 16:15:54 -0300

    Render project images

--- src/pages/works/project-images.jsx
+++ src/pages/works/project-images.jsx
@@ -1,3 +1,18 @@
 import React from 'react';
+import { Image, SimpleGrid } from '@chakra-ui/react';
 
-export default function ProjectImages() {}
+export default function ProjectImages({
+	preview,
+	mobilePreview1,
+	mobilePreview2,
+}) {
+	return (
+		<>
+			<Image src={preview} borderRadius="md" />
+			<SimpleGrid columns={2} mt={3} gap={3}>
+				<Image src={mobilePreview1} borderRadius="md" />
+				<Image src={mobilePreview2} borderRadius="md" />
+			</SimpleGrid>
+		</>
+	);
+}
