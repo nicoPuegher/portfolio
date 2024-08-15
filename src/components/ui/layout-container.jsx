95084891691944c04b9dd5e14381c99f25e055ba
Nicolas <nicopuegher@gmail.com>
2024-08-15 16:00:45 -0300

    Render a custom container from the layout-container ui component

--- src/components/ui/layout-container.jsx
+++ src/components/ui/layout-container.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
+import { Container } from '@chakra-ui/react';
 
-export default function LayoutContainer() {}
+export default function LayoutContainer({ as = 'div', children, ...props }) {
+	return (
+		<Container as={as} maxW="container.md" p={2} {...props}>
+			{children}
+		</Container>
+	);
+}
