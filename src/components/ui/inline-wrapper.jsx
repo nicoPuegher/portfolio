005355885735706a4a0faa9c58cde8e2895be990
Nicolas <nicopuegher@gmail.com>
2024-08-17 21:18:54 -0300

    Render a box component and add proptypes for the inline wrapper component

--- src/components/ui/inline-wrapper.jsx
+++ src/components/ui/inline-wrapper.jsx
@@ -1,1 +1,10 @@
-export default function InlineWrapper() {}
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+
+export default function InlineWrapper({ children }) {
+	return <Box>{children}</Box>;
+}
+InlineWrapper.propTypes = {
+	children: PropTypes.node.isRequired,
+};
