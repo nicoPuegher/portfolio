dbf766d445c5087e9fc888e5a411947e817a5c89
Nicolas <nicopuegher@gmail.com>
2024-08-17 21:32:00 -0300

    Render the new inline wrapper instead of a default box in the header layout

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 
 import * as Navbar from '@components/navbar';
+import InlineWrapper from '@/components/ui/inline-wrapper';
 
 export default function Header() {
 	return (
 		<Box as="header" pos="sticky" top={0} bg="black" zIndex="sticky">
 			<Navbar.Nav>
 				<Navbar.Logo />
-				<Box>
+				<InlineWrapper>
 					<Navbar.ThemeToggle />
 					<Navbar.MenuMobile />
-				</Box>
+				</InlineWrapper>
 			</Navbar.Nav>
 		</Box>
 	);
