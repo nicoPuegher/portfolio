b6a2e05de2e10ad9aa86864217df8ce7e1f409cc
Nicolas <nicopuegher@gmail.com>
2024-08-20 15:23:17 -0300

    Update import and use of custom flex component in header component

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 
 import * as Navbar from '@components/navbar';
-import InlineWrapper from '@/components/ui/inline-wrapper';
+import CustomFlex from '@/components/ui/custom-flex';
 
 export default function Header() {
 	return (
 		<Box as="header" pos="sticky" top={0} bg="black" zIndex="sticky">
 			<Navbar.Nav>
 				<Navbar.Logo />
-				<InlineWrapper>
+				<CustomFlex>
 					<Navbar.ThemeToggle />
 					<Navbar.MenuMobile />
-				</InlineWrapper>
+				</CustomFlex>
 			</Navbar.Nav>
 		</Box>
 	);
