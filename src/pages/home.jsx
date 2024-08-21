25d966dc5dfae4cf55c52741f637d808e2bff0b3
Nicolas <nicopuegher@gmail.com>
2024-08-21 15:54:18 -0300

    Add the greetings component directly inside home layout, no extra component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Divider, Text } from '@chakra-ui/react';
 
-import Section from '@/layouts/section';
+import CustomGrid from '@/components/ui/custom-grid';
 import Message from '@/components/message';
-import Greetings from '@/components/greetings';
+import Profile from '@/components/profile';
+import Myself from '@/components/myself';
+import ButtonCall from '@/components/ui/button-call';
+import Section from '@/layouts/section';
 import developer from '@/constants/developer';
 
 export default function Home() {
 	return (
-		<>
+		<CustomGrid>
 			<Message>{developer.greeting}</Message>
-			<Greetings />
+			<Profile />
+			<Myself />
+			<ButtonCall text="Check my work" />
 			<Divider width="half" mx="auto" my={10} />
 			<Section title="Quick summary ">
 				<Text>{developer.quickSummary}</Text>
 			</Section>
-		</>
+		</CustomGrid>
 	);
 }
