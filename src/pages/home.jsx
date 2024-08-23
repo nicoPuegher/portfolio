c06fd9b9eaddc0ab071b0d6eab3f73a361ec9b81
Nicolas <nicopuegher@gmail.com>
2024-08-23 17:41:24 -0300

    Add new custom-grid and custom-section components to the home layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,28 +5,30 @@ import Context from '@/store/context';
 import CustomGrid from '@/components/ui/custom-grid';
 import Message from '@/components/message';
 import Profile from '@/components/profile';
+import CustomSection from '@/components/ui/custom-section';
 import ButtonCall from '@/components/ui/button-call';
-import Section from '@/layouts/section';
 import developer from '@/constants/developer';
 
 export default function Home() {
 	const context = useContext(Context);
 
 	return (
-		<CustomGrid>
+		<CustomGrid customVariant="external">
 			<Message>{developer.greeting}</Message>
-			<Profile />
-			<Section title={developer.name}>
-				<Text>{developer.role}</Text>
-			</Section>
-			<ButtonCall
-				text="Check my work"
-				goToPage={() => context.handleChangePage('works')}
-			/>
+			<CustomGrid>
+				<Profile />
+				<CustomSection title={developer.name}>
+					<Text>{developer.role}</Text>
+				</CustomSection>
+				<ButtonCall
+					text="Check my work"
+					goToPage={() => context.handleChangePage('works')}
+				/>
+			</CustomGrid>
 			<Divider width="half" />
-			<Section title="Quick summary ">
+			<CustomSection title="Quick summary ">
 				<Text>{developer.quickSummary}</Text>
-			</Section>
+			</CustomSection>
 		</CustomGrid>
 	);
 }
