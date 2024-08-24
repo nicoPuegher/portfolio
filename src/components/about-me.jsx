788000eced967292108ca1067e4a87e1337d930e
Nicolas <nicopuegher@gmail.com>
2024-08-24 18:07:52 -0300

    Use the custom-grid component in the about-me component

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -3,12 +3,14 @@ import { Text } from '@chakra-ui/react';
 
 import developer from '@/constants/developer';
 
+import CustomGrid from './ui/custom-grid';
+
 export default function AboutMe() {
 	return (
-		<>
-			<Text mb={5}>{developer.summary['1']}</Text>
-			<Text mb={5}>{developer.summary['2']}</Text>
+		<CustomGrid>
+			<Text>{developer.summary['1']}</Text>
+			<Text>{developer.summary['2']}</Text>
 			<Text>{developer.summary['3']}</Text>
-		</>
+		</CustomGrid>
 	);
 }
