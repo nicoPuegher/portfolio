7dc4f96427e401c7d5fecf41b938874ab92cfd98
Nicolas <nicopuegher@gmail.com>
2024-08-24 18:06:55 -0300

    Use the custom-grid and custom-section components in the about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Divider } from '@chakra-ui/react';
 
-import Section from '@/layouts/section';
+import CustomGrid from '@/components/ui/custom-grid';
+import CustomSection from '@/components/ui/custom-section';
 import AboutMe from '@/components/about-me';
 import MyExperience from '@/components/my-experience';
 
 export default function About() {
 	return (
-		<>
-			<Section title="About me">
+		<CustomGrid customVariant="external">
+			<CustomSection title="About me">
 				<AboutMe />
-			</Section>
-			<Divider width="half" mx="auto" my={10} />
-			<Section title="My experience">
+			</CustomSection>
+			<Divider width="half" />
+			<CustomSection title="My experience">
 				<MyExperience />
-			</Section>
-		</>
+			</CustomSection>
+		</CustomGrid>
 	);
 }
