c6b8a4ff29656bb515a732ff62599506745c967c
Nicolas <nicopuegher@gmail.com>
2024-08-24 18:49:28 -0300

    Move the about-me component directly inside about page component

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Divider } from '@chakra-ui/react';
+import { Text, Divider } from '@chakra-ui/react';
 
 import CustomGrid from '@/components/ui/custom-grid';
 import CustomSection from '@/components/ui/custom-section';
-import AboutMe from '@/components/about-me';
 import MyExperience from '@/components/my-experience';
+import developer from '@/constants/developer';
 
 export default function About() {
 	return (
 		<CustomGrid customVariant="external">
 			<CustomSection title="About me">
-				<AboutMe />
+				<CustomGrid>
+					{Object.values(developer.summary).map((paragraph) => (
+						<Text key={paragraph}>{paragraph}</Text>
+					))}
+				</CustomGrid>
 			</CustomSection>
 			<Divider width="half" />
 			<CustomSection title="My experience">
