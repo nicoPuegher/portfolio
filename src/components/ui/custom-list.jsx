1e80c0e7f5fb2ad927329c255081327931c998a3
Nicolas <nicopuegher@gmail.com>
2024-08-24 18:19:55 -0300

    Render an unordered list and custom-list items in the custom-list component

--- src/components/ui/custom-list.jsx
+++ src/components/ui/custom-list.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
+import { UnorderedList, ListItem, Text } from '@chakra-ui/react';
 
-export default function CustomList() {}
+export default function CustomList({ list }) {
+	return (
+		<UnorderedList spacing={2.5}>
+			{list.map((item) => (
+				<ListItem key={item} fontSize="sm">
+					<Text as="i">{item}</Text>
+				</ListItem>
+			))}
+		</UnorderedList>
+	);
+}
