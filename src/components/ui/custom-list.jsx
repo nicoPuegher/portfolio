8e74e8d822968af4c45e5cb61623f342ca7f0f8a
Nicolas <nicopuegher@gmail.com>
2024-08-28 21:34:51 -0300

    Add a variant by prop to custom-list ui component

--- src/components/ui/custom-list.jsx
+++ src/components/ui/custom-list.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import { UnorderedList, ListItem, Text } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-export default function CustomList({ list }) {
+export default function CustomList({ list, features = 'false' }) {
 	return (
 		<UnorderedList pr={4} spacing={2.5} listStylePos="inside">
-			{list.map((item) => (
-				<ListItem key={item} fontSize="sm">
-					<Text as="i">{item}</Text>
-				</ListItem>
-			))}
+			{list.map((item) => {
+				if (features) {
+					return (
+						<ListItem key={item.title} fontSize="sm">
+							<Text as="i">
+								<Text as="b">{item.title}</Text>
+								{item.content}
+							</Text>
+						</ListItem>
+					);
+				}
+				return (
+					<ListItem key={item} fontSize="sm">
+						<Text as="i">{item}</Text>
+					</ListItem>
+				);
+			})}
 		</UnorderedList>
 	);
 }
 CustomList.propTypes = {
 	list: PropTypes.instanceOf(Array).isRequired,
+	features: PropTypes.string,
 };
