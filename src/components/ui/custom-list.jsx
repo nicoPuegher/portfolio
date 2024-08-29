a88b1414702374eb421bba994638e420fc540b99
Nicolas <nicopuegher@gmail.com>
2024-08-29 20:07:20 -0300

    Simplify the feature variant of the custom-list ui component

--- src/components/ui/custom-list.jsx
+++ src/components/ui/custom-list.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { UnorderedList, ListItem, Text } from '@chakra-ui/react';
+import { ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-export default function CustomList({ list, features = 'false' }) {
+export default function CustomList({ variant = 'text', list }) {
+	const customList = list.map((item) => {
+		if (variant === 'features') {
+			return (
+				<ListItem key={item.title} fontSize="sm">
+					<Text as="i">
+						<Text as="b">{item.title}</Text>
+						{item.content}
+					</Text>
+				</ListItem>
+			);
+		}
+	});
+
 	return (
-		<UnorderedList pr={4} spacing={2.5} listStylePos="inside">
-			{list.map((item) => {
-				if (features) {
-					return (
-						<ListItem key={item.title} fontSize="sm">
-							<Text as="i">
-								<Text as="b">{item.title}</Text>
-								{item.content}
-							</Text>
-						</ListItem>
-					);
-				}
-				return (
-					<ListItem key={item} fontSize="sm">
-						<Text as="i">{item}</Text>
-					</ListItem>
-				);
-			})}
+		<UnorderedList
+			pr={4}
+			spacing={2.5}
+			listStylePos="inside"
+			listStyleType={variant === 'projectDetails' ? 'none' : 'disc'}
+		>
+			{customList}
 		</UnorderedList>
 	);
 }
 CustomList.propTypes = {
+	variant: PropTypes.string,
 	list: PropTypes.instanceOf(Array).isRequired,
-	features: PropTypes.string,
 };
