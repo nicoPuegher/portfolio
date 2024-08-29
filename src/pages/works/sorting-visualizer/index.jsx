b5568029d411a61f366b0798289de8459772e98e
Nicolas <nicopuegher@gmail.com>
2024-08-28 21:23:15 -0300

    Add the custom-grid component as a content wrapper in the sorting-visualizer component

--- src/pages/works/sorting-visualizer/index.jsx
+++ src/pages/works/sorting-visualizer/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { List } from '@chakra-ui/react';
 
 import CustomSection from '@/components/ui/custom-section';
+import CustomGrid from '@/components/ui/custom-grid';
 import sortingVisualizer from '@/constants/projects/sorting-visualizer';
 import Breadcrumb from '@/pages/works/breadcrumb';
 import ProjectDescription from '@/pages/works/project-description';
@@ -12,21 +13,23 @@ import ProjectImages from '@/pages/works/project-images';
 export default function SortingVisualizer() {
 	return (
 		<CustomSection title={sortingVisualizer.title}>
-			<Breadcrumb
-				name={sortingVisualizer.title}
-				year={sortingVisualizer.year}
-			/>
-			<ProjectDescription project={sortingVisualizer} />
-			<List>
-				<ProjectLinks url={sortingVisualizer.link} />
-				<ProjectTechnologies stack={sortingVisualizer.stack} />
-			</List>
-			<ProjectImages
-				preview={sortingVisualizer.preview}
-				mobilePreview1={sortingVisualizer.mobilePreviews[0]}
-				mobilePreview2={sortingVisualizer.mobilePreviews[1]}
-				fallback={sortingVisualizer.fallback}
-			/>
+			<CustomGrid>
+				<Breadcrumb
+					name={sortingVisualizer.title}
+					year={sortingVisualizer.year}
+				/>
+				<ProjectDescription project={sortingVisualizer} />
+				<List>
+					<ProjectLinks url={sortingVisualizer.link} />
+					<ProjectTechnologies stack={sortingVisualizer.stack} />
+				</List>
+				<ProjectImages
+					preview={sortingVisualizer.preview}
+					mobilePreview1={sortingVisualizer.mobilePreviews[0]}
+					mobilePreview2={sortingVisualizer.mobilePreviews[1]}
+					fallback={sortingVisualizer.fallback}
+				/>
+			</CustomGrid>
 		</CustomSection>
 	);
 }
