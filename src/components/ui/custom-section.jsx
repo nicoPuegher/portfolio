80c09072032597d6d15761bcd1f53b17e4ad1cb3
Nicolas <nicopuegher@gmail.com>
2024-08-30 12:29:13 -0300

    Add a dynamic badge component to custom-section ui component

--- src/components/ui/custom-section.jsx
+++ src/components/ui/custom-section.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Badge } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
 export default function CustomSection({
 	customVariant = 'h2',
 	title,
+	badge = null,
 	children,
 }) {
 	const as = customVariant === 'h2' ? 'h2' : 'h3';
@@ -12,6 +13,7 @@ export default function CustomSection({
 
 	return (
 		<Box as="section" textAlign="center">
+			{badge && <Badge>{badge}</Badge>}
 			<Heading as={as} size={size} mb={1}>
 				{title}
 			</Heading>
@@ -22,5 +24,6 @@ export default function CustomSection({
 CustomSection.propTypes = {
 	customVariant: PropTypes.string,
 	title: PropTypes.string.isRequired,
+	badge: PropTypes.string,
 	children: PropTypes.node.isRequired,
 };
