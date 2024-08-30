72f36289cd631247989b413fd6ac56bcda8564e9
Nicolas <nicopuegher@gmail.com>
2024-08-30 11:47:53 -0300

    Update expenses-tracker component to reflect all new changes from sorting-visualizer component

--- src/pages/works/expenses-tracker/index.jsx
+++ src/pages/works/expenses-tracker/index.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
-import { List } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
-import Section from '@/layouts/section';
+import CustomSection from '@/components/ui/custom-section';
+import CustomGrid from '@/components/ui/custom-grid';
 import expensesTracker from '@/constants/projects/expenses-tracker';
 import Breadcrumb from '@/pages/works/breadcrumb';
-import ProjectDescription from '@/pages/works/project-description';
-import ProjectLinks from '@/pages/works/project-links';
-import ProjectTechnologies from '@/pages/works/project-technologies';
+import CustomList from '@/components/ui/custom-list';
 import ProjectImages from '@/pages/works/project-images';
 
 export default function ExpensesTracker() {
 	return (
-		<Section title={expensesTracker.title}>
-			<Breadcrumb
-				name={expensesTracker.title}
-				year={expensesTracker.year}
-			/>
-			<ProjectDescription project={expensesTracker} />
-			<List>
-				<ProjectLinks url={expensesTracker.link} />
-				<ProjectTechnologies stack={expensesTracker.stack} />
-			</List>
-			<ProjectImages
-				title={expensesTracker.title}
-				preview={expensesTracker.preview}
-				mobilePreview1={expensesTracker.mobilePreviews[0]}
-				mobilePreview2={expensesTracker.mobilePreviews[1]}
-				fallback={expensesTracker.fallback}
-			/>
-		</Section>
+		<CustomSection title={expensesTracker.title}>
+			<CustomGrid>
+				<Breadcrumb
+					name={expensesTracker.title}
+					year={expensesTracker.year}
+				/>
+				<Text>{expensesTracker.description}</Text>
+				<CustomList
+					variant="features"
+					list={expensesTracker.features}
+				/>
+				<Text>{expensesTracker.closer}</Text>
+				<CustomList
+					variant="projectDetails"
+					list={expensesTracker.details}
+					stack={expensesTracker.stack}
+				/>
+				<ProjectImages
+					preview={expensesTracker.preview}
+					mobilePreview1={expensesTracker.mobilePreviews[0]}
+					mobilePreview2={expensesTracker.mobilePreviews[1]}
+					fallback={expensesTracker.fallback}
+				/>
+			</CustomGrid>
+		</CustomSection>
 	);
 }
