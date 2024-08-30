bfd31153fa70de546ebddedceb9bbb95d3f159b4
Nicolas <nicopuegher@gmail.com>
2024-08-30 11:04:31 -0300

    Remove unnecessary imports in sorting-visualizer component

--- src/pages/works/sorting-visualizer/index.jsx
+++ src/pages/works/sorting-visualizer/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Text, List } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
 import CustomSection from '@/components/ui/custom-section';
 import CustomGrid from '@/components/ui/custom-grid';
 import sortingVisualizer from '@/constants/projects/sorting-visualizer';
 import Breadcrumb from '@/pages/works/breadcrumb';
 import CustomList from '@/components/ui/custom-list';
-import ProjectLinks from '@/pages/works/project-links';
-import ProjectTechnologies from '@/pages/works/project-technologies';
 import ProjectImages from '@/pages/works/project-images';
 
 export default function SortingVisualizer() {
