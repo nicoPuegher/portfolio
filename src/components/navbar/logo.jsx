59441c9feffb02fef90bf6907e1aafd5483fb8ee
Nicolas <nicopuegher@gmail.com>
2024-08-30 15:22:36 -0300

    Clean up the logo component and use the custom-button ui component

--- src/components/navbar/logo.jsx
+++ src/components/navbar/logo.jsx
@@ -1,24 +1,23 @@
 import React, { useContext } from 'react';
-import { Heading, Button } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 
 import Context from '@/store/context';
+import CustomButton from '@/components/ui/custom-button';
 import developer from '@constants/developer';
 
 export default function Logo() {
 	const context = useContext(Context);
 
-	const handleClick = () => context.handleChangePage('home');
-
 	return (
 		<Heading as="h1" display="inline-flex">
-			<Button
+			<CustomButton
 				variant="link"
 				fontSize="xl"
 				color="white"
-				onClick={handleClick}
+				onClick={() => context.handleChangePage('home')}
 			>
 				{developer.name}
-			</Button>
+			</CustomButton>
 		</Heading>
 	);
 }
