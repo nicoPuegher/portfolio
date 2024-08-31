ca0d38ea9539a23be42fbac12316fce0473dbb07
Nicolas <nicopuegher@gmail.com>
2024-08-31 19:49:39 -0300

    Render an image with custom props in custom-image ui component

--- src/components/ui/custom-image.jsx
+++ src/components/ui/custom-image.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
+import { Image, Center } from '@chakra-ui/react';
 
-export default function CustomImage() {}
+export default function CustomImage({ src, alt }) {
+	return (
+		<Image
+			src={src}
+			alt={alt}
+			fallback={<Center>Loading image...</Center>}
+			borderRadius="md"
+		/>
+	);
+}
