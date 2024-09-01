73bfda7ed704ca0cf817da09df0b135258d65d9b
Nicolas <nicopuegher@gmail.com>
2024-09-01 20:16:28 -0300

    Add a ratio prop in the custom-image ui component

--- src/components/ui/custom-image.jsx
+++ src/components/ui/custom-image.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Image, Center } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-export default function CustomImage({ src, alt = 'Application preview' }) {
+export default function CustomImage({
+	src,
+	alt = 'Application preview',
+	ratio = 'desktop',
+}) {
+	const customRatio = ratio === 'desktop' ? 16 / 10 : 9 / 16;
+
 	return (
 		<Image
 			src={src}
@@ -15,4 +21,5 @@ export default function CustomImage({ src, alt = 'Application preview' }) {
 CustomImage.propTypes = {
 	src: PropTypes.string.isRequired,
 	alt: PropTypes.string,
+	ratio: PropTypes.string,
 };
