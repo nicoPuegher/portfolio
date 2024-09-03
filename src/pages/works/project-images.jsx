2238681aec662fb78f57c6d32eee432727e6dc95
Nicolas <nicopuegher@gmail.com>
2024-09-03 13:53:55 -0300

    Use the custom-image ui component in project-images component and simplify the render of images

--- src/pages/works/project-images.jsx
+++ src/pages/works/project-images.jsx
@@ -1,48 +1,30 @@
 import React from 'react';
-import { AspectRatio, Image, Center, GridItem } from '@chakra-ui/react';
+import { GridItem } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
 import CustomGrid from '@/components/ui/custom-grid';
+import CustomImage from '@/components/ui/custom-image';
 
 export default function ProjectImages({
 	previewImage,
 	mobilePreview1,
 	mobilePreview2,
-	imageFallback,
 }) {
+	const imagesArray = [previewImage, mobilePreview1, mobilePreview2];
+
+	const images = imagesArray.map((src, index) => (
+		<GridItem key={src} w="full" colSpan={index === 0 ? 2 : 1}>
+			<CustomImage src={src} ratio={index === 0 ? 'desktop' : 'mobile'} />
+		</GridItem>
+	));
+
 	return (
 		<CustomGrid
 			w="full"
 			templateRows="repeat(2, auto)"
 			templateColumns="repeat(2, 1fr)"
 		>
-			<GridItem w="full" colSpan={2}>
-				<AspectRatio borderRadius="md" shadow="xs" ratio={16 / 10}>
-					<Image
-						src={previewImage}
-						borderRadius="md"
-						fallback={<Center>{imageFallback}</Center>}
-					/>
-				</AspectRatio>
-			</GridItem>
-			<GridItem w="full">
-				<AspectRatio borderRadius="md" shadow="xs" ratio={9 / 16}>
-					<Image
-						src={mobilePreview1}
-						borderRadius="md"
-						fallback={<Center>{imageFallback}</Center>}
-					/>
-				</AspectRatio>
-			</GridItem>
-			<GridItem w="full">
-				<AspectRatio borderRadius="md" shadow="xs" ratio={9 / 16}>
-					<Image
-						src={mobilePreview2}
-						borderRadius="md"
-						fallback={<Center>{imageFallback}</Center>}
-					/>
-				</AspectRatio>
-			</GridItem>
+			{images}
 		</CustomGrid>
 	);
 }
@@ -50,5 +32,4 @@ ProjectImages.propTypes = {
 	previewImage: PropTypes.string.isRequired,
 	mobilePreview1: PropTypes.string.isRequired,
 	mobilePreview2: PropTypes.string.isRequired,
-	imageFallback: PropTypes.string.isRequired,
 };
