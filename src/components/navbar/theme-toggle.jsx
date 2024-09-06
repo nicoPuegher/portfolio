2ffa0f6ca0c326f959f968261748d263a2fe845c
Nicolas <nicopuegher@gmail.com>
2024-09-06 17:23:18 -0300

    Update colors for light theme only in theme-toggle component

--- src/components/navbar/theme-toggle.jsx
+++ src/components/navbar/theme-toggle.jsx
@@ -2,25 +2,18 @@ import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
-import colors from '@/lib/colors';
+import { dark } from '@/lib/colors';
 
 export default function ThemeToggle() {
 	const { colorMode, toggleColorMode } = useColorMode();
 
-	const moon = (
-		<MoonIcon color={useColorModeValue(colors.black, colors.white)} />
-	);
-	const sun = (
-		<SunIcon color={useColorModeValue(colors.black, colors.white)} />
-	);
+	const moon = <MoonIcon color={useColorModeValue(dark.black, '')} />;
+	const sun = <SunIcon color={useColorModeValue(dark.black, '')} />;
 
 	return (
 		<IconButton
 			icon={colorMode === 'light' ? moon : sun}
-			colorScheme={useColorModeValue(
-				colors.lightAction,
-				colors.darkAction,
-			)}
+			colorScheme={useColorModeValue(dark.active, '')}
 			onClick={toggleColorMode}
 		/>
 	);
