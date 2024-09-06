37e27da0e49d1f5147c4085d47a1fea6375cf534
Nicolas <nicopuegher@gmail.com>
2024-09-06 14:53:55 -0300

    Move the config direclty inside the theme config instead of an external constant in theme

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -2,13 +2,11 @@ import { extendTheme } from '@chakra-ui/react';
 
 import Container from './container-theme';
 
-const config = {
-	initialColorMode: 'system',
-	useSystemColorMode: true,
-};
-
 const theme = extendTheme({
-	config,
+	config: {
+		initialColorMode: 'system',
+		useSystemColorMode: true,
+	},
 	fonts: {
 		heading: 'Inter, sans-serif',
 		body: 'Inter, sans-serif',
