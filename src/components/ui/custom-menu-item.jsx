679efd74b16ba1647a56b9f352de7de011e0b395
Nicolas <nicopuegher@gmail.com>
2024-09-06 17:24:09 -0300

    Update colors for light theme only in custom-menu-item component

--- src/components/ui/custom-menu-item.jsx
+++ src/components/ui/custom-menu-item.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { MenuItem } from '@chakra-ui/react';
+import { useColorModeValue, MenuItem } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
+
+import { dark } from '@/lib/colors';
 
 export default function CustomMenuItem({ children, ...props }) {
 	return (
-		<MenuItem letterSpacing="inherit" {...props}>
+		<MenuItem
+			letterSpacing="inherit"
+			color={useColorModeValue(dark.black, '')}
+			bg={useColorModeValue(dark.white, '')}
+			_focus={{ bg: useColorModeValue(dark.focus, '') }}
+			{...props}
+		>
 			{children}
 		</MenuItem>
 	);
