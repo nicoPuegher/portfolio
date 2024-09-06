f0a2c27638ba3adb07964cb041dc7d5ad15b471b
Nicolas <nicopuegher@gmail.com>
2024-09-06 19:06:34 -0300

    Add colors for light theme only in footer layout

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { useColorModeValue, Text } from '@chakra-ui/react';
 
 import CustomContainer from '@/components/ui/custom-container';
+import { dark } from '@/lib/colors';
 
 export default function Footer() {
 	const date = new Date();
 	const currentYear = date.getFullYear().toString();
 
 	return (
-		<CustomContainer as="footer" bg="black">
-			<Text fontSize="sm" textAlign="center" color="white">
+		<CustomContainer
+			as="footer"
+			color={useColorModeValue(dark.white, '')}
+			bg={useColorModeValue(dark.black, '')}
+		>
+			<Text fontSize="sm" textAlign="center">
 				© <time dateTime={currentYear}>{currentYear}</time> - All
 				rights reserved.
 			</Text>
