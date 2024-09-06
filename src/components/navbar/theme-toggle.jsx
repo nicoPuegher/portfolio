18f5968187b7d0325e72a490deb2073304b8efc4
Nicolas <nicopuegher@gmail.com>
2024-09-06 16:03:18 -0300

    Add a custom color scheme for the theme-toggle component based on theme

--- src/components/navbar/theme-toggle.jsx
+++ src/components/navbar/theme-toggle.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { useColorMode, IconButton } from '@chakra-ui/react';
+import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+import colors from '@/lib/colors';
 
 export default function ThemeToggle() {
 	const { colorMode, toggleColorMode } = useColorMode();
 
+	const moon = (
+		<MoonIcon color={useColorModeValue(colors.black, colors.white)} />
+	);
+	const sun = (
+		<SunIcon color={useColorModeValue(colors.black, colors.white)} />
+	);
+
 	return (
 		<IconButton
-			icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+			icon={colorMode === 'light' ? moon : sun}
+			colorScheme={useColorModeValue(
+				colors.lightAction,
+				colors.darkAction,
+			)}
 			onClick={toggleColorMode}
 		/>
 	);
