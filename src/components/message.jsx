c6d75b3f578a13d3461b7fe12bf8b938c1283cb7
Nicolas <nicopuegher@gmail.com>
2024-09-06 17:28:39 -0300

    Add colors for light theme only in message component

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { useColorModeValue, Text } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
+
+import { dark } from '@/lib/colors';
 
 import CustomContainer from './ui/custom-container';
 
 export default function Message({ children }) {
 	return (
-		<CustomContainer borderRadius="md" color="white" bg="black">
+		<CustomContainer
+			borderRadius="md"
+			color={useColorModeValue(dark.white, '')}
+			bg={useColorModeValue(dark.black, '')}
+		>
 			<Text textAlign="center">{children}</Text>
 		</CustomContainer>
 	);
