7c38ccf47b8abf93410d86246e0d6c473c7a911d
Nicolas <nicopuegher@gmail.com>
2024-09-07 16:03:46 -0300

    Update light theme colors in custom-badge ui component

--- src/components/ui/custom-badge.jsx
+++ src/components/ui/custom-badge.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import { useColorModeValue, Badge } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-import { dark } from '@/lib/colors';
+import { light, dark } from '@/lib/colors';
 
 export default function CustomBadge({ text }) {
 	return (
-		<Badge
-			color={useColorModeValue(dark.black, '')}
-			colorScheme={useColorModeValue(dark.active, '')}
-		>
-			{text}
-		</Badge>
+		<Badge colorScheme={useColorModeValue(light.primary, '')}>{text}</Badge>
 	);
 }
 CustomBadge.propTypes = {
