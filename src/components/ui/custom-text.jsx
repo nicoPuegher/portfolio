a0f5022fe99fd558eeb171c93a51497bb9d00fc6
Nicolas <nicopuegher@gmail.com>
2024-09-08 16:11:26 -0300

    Update light theme colors in custom-text ui component

--- src/components/ui/custom-text.jsx
+++ src/components/ui/custom-text.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { useColorModeValue, Text } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-import { dark } from '@/lib/colors';
-
 export default function CustomText({ children }) {
-	return (
-		<Text as="i" color={useColorModeValue(dark.dimmedBlack, '')}>
-			{children}
-		</Text>
-	);
+	return <Text as="i">{children}</Text>;
 }
 CustomText.propTypes = {
 	children: PropTypes.node.isRequired,
