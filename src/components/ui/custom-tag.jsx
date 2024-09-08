c54fb2eb4bc4fa48f4926eaf5f23b0b7ab9ceae0
Nicolas <nicopuegher@gmail.com>
2024-09-08 16:11:04 -0300

    Update light theme colors in custom-tag ui component

--- src/components/ui/custom-tag.jsx
+++ src/components/ui/custom-tag.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 import { useColorModeValue, Tag } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
-import { dark } from '@/lib/colors';
+import { light, dark } from '@/lib/colors';
 
 export default function CustomTag({ text }) {
-	return (
-		<Tag
-			fontWeight="normal"
-			color={useColorModeValue(dark.dimmedBlack, '')}
-		>
-			{text}
-		</Tag>
-	);
+	return <Tag fontWeight="normal">{text}</Tag>;
 }
 CustomTag.propTypes = {
 	text: PropTypes.string.isRequired,
