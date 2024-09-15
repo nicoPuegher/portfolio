c6c433fb5c99d7dda50bd430346160ab6522c0b4
Nicolas <nicopuegher@gmail.com>
2024-09-15 20:15:31 -0300

    Update colors with new light theme in menu component

--- src/components/navbar/menu.jsx
+++ src/components/navbar/menu.jsx
@@ -1,27 +1,45 @@
 import React, { useContext } from 'react';
-import { ButtonGroup, Button } from '@chakra-ui/react';
+import { useColorModeValue, ButtonGroup, Link } from '@chakra-ui/react';
 
 import Context from '@/store/context';
+import CustomFlex from '@/components/ui/custom-flex';
+import CustomButton from '@/components/ui/custom-button';
+import menu from '@/constants/menu';
+import { light, dark } from '@/lib/colors';
 
 export default function Menu() {
 	const context = useContext(Context);
 
+	const buttonColor = useColorModeValue(light.text_light_content, '');
+	const buttonBg = useColorModeValue(light.neutral, '');
+
 	return (
-		<ButtonGroup display="none" spacing={3} variant="ghost">
-			<Button
-				letterSpacing="inherit"
-				onClick={() => context.handleChangePage('about')}
-			>
-				About
-			</Button>
-			<Button
-				letterSpacing="inherit"
-				onClick={() => context.handleChangePage('works')}
-			>
-				Works
-			</Button>
-			<Button letterSpacing="inherit">Linkedin</Button>
-			<Button letterSpacing="inherit">Github</Button>
+		<ButtonGroup display={{ base: 'none', lg: 'flex' }}>
+			<CustomFlex align="center">
+				{menu.buttons.map((name) => (
+					<CustomButton
+						key={name}
+						variant="ghost"
+						text={name}
+						color={buttonColor}
+						_hover={{ bg: buttonBg }}
+						onClick={() =>
+							context.handleChangePage(name.toLowerCase())
+						}
+					/>
+				))}
+
+				{menu.links.map((link) => (
+					<Link key={link.name} href={link.url}>
+						<CustomButton
+							variant="ghost"
+							text={link.name}
+							color={buttonColor}
+							_hover={{ bg: buttonBg }}
+						/>
+					</Link>
+				))}
+			</CustomFlex>
 		</ButtonGroup>
 	);
 }
