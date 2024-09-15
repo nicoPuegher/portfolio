47845ccf68509ed6cbb916b582108aef89c90f46
Nicolas <nicopuegher@gmail.com>
2024-09-15 20:13:08 -0300

    Create a custom-active-button ui component

---
+++ src/components/ui/custom-active-button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useColorModeValue } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+import PropTypes from 'prop-types';
+
+import { light, dark } from '@/lib/colors';
+
+import CustomButton from './custom-button';
+
+export default function CustomActiveButton({ text, ...props }) {
+	return (
+		<CustomButton
+			rightIcon={<ArrowForwardIcon />}
+			text={text}
+			color={useColorModeValue(light.text_light_content, '')}
+			colorScheme={useColorModeValue(light.primary, '')}
+			{...props}
+		/>
+	);
+}
+CustomActiveButton.propTypes = {
+	text: PropTypes.string.isRequired,
+};
